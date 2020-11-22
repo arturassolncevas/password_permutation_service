a3ab513930c71d7885efdd27f2da23be75866fed
Arturas Solncevas <as@centic.dk>
2020-11-22 11:53:32 +0100

    Modify api passwords controller to perform password checkup

--- src/controllers/api/passwords-controller.js
+++ src/controllers/api/passwords-controller.js
@@ -1,14 +1,27 @@
-import { validations } from '../../helpers/validators/password-check-validator'
-import { validationResult } from 'express-validator'
+import validations from '../../helpers/validators/password-check-validator'
+import PasswordManager, { normalize, compare } from "../../services/managers/password-manager"
+import UserManager from "../../services/managers/user-manager"
+
+const userManager = new UserManager()
+const passwordManager = new PasswordManager()
 
 export const checkPassword = [
-    validations,
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        res.json({ message: "check password" })
-    }
-].flat()
+    ...validations,
+    async (req, res) => {
+        let message = { success: false }
+        let { username, password } = req.body
+        let user = await userManager.findByUserName(username)
+        password = normalize(password)
 
+        if (!user) {
+            user = await userManager.createUser({username})
+            await passwordManager.createMutation({userId: user.id, password })
+        } else {
+            let passwordHash = await passwordManager.getLastMutationHash(user.id)
+            if (!compare(password, passwordHash)) {
+                await passwordManager.createMutation({userId: user.id, password })
+            } else { message.success = true }
+        }
+        res.json(message)
+    }
+]
