88fec5a62ef3eb9be737e97254b6ad4f28dda10f
Arturas Solncevas <as@centic.dk>
2020-11-22 12:08:55 +0100

    Add last updates to api routes passwords

--- src/routes/api/passwords.js
+++ src/routes/api/passwords.js
@@ -1,9 +1,10 @@
-import { checkPassword } from '../../controllers/api/passwords-controller'
+import {checkPassword, lastUpdates} from '../../controllers/api/passwords-controller'
 import express from 'express'
 
 const router = express.Router()
 const path = '/passwords'
 
 router.post(`${path}/check`, checkPassword)
+router.get(`${path}/last-updates`, lastUpdates)
 
 export default router
